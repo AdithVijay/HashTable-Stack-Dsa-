8a642c8ae5105832ad0cb9478a6a94ae8e2ed708
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-12-11 19:06:19 +0530

    chNges

---
+++ Stack/stack.js
@@ -0,0 +1,47 @@
+class Stack{
+    constructor(){
+        this.item =[]
+        this.count = 0
+    }
+
+    push(data){
+        this.item.push(data)
+        this.count++
+    }
+    pop(){
+        if(this.isempty()){
+            return
+        }
+        this.item.pop()
+        this.count-=1
+    }
+
+    size(){
+        return this.item.length
+    }
+
+    peek(){
+        if(this.size){
+            console.log("kunjij iri");
+            return
+        }
+        return this.item[this.size()-1]
+    }
+
+    isempty(){
+        return this.item.length==0 
+    }
+}
+const newStack = new Stack()
+
+
+newStack.push(2)
+newStack.push(2)
+newStack.push(1000)
+newStack.pop()
+newStack.pop()
+newStack.pop()
+console.log("this is peek",newStack.peek());
+// newStack.size()
+
+console.log(newStack)
---
+++ Stack/stakusingLinkedlist.js
@@ -0,0 +1,38 @@
+class Node {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+}
+class  Stack{
+    constructor() {
+        this.head = null
+        this.size = 0
+    }
+    push(data){
+        const newNode = new Node(data)
+        newNode.next = this.head
+        this.head = newNode
+        this.size++
+    }
+    pop(){
+        this.head = this.head.next
+    }
+    print(){
+        let current = this.head
+        let arr = []    
+        while(current){ 
+            arr.push(current.data)
+            current=current.next
+        }
+        console.log(arr.join("=>"),"null")
+    }
+}
+const stack = new Stack()
+stack.push(3)
+stack.push(2)
+stack.push(1)
+stack.push(0)
+stack.pop()
+stack.print()
+console.log(stack);
