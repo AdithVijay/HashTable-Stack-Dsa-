f33eb8db59231b18e1e9b1ea1a72172c106f2519
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-12-13 14:50:25 +0530

    changes

--- hashtables/p.js
+++ hashtables/p.js
@@ -1,52 +1,5 @@
 class HashTable {
-    constructor(size = 10) {
-        this.table = Array(size)
-    }
-    hash(key){
-        let hash=0
-        for(let char of key){
-            hash+= char.charCodeAt(0)
-        }
-        return hash % this.table.length
-    }
-
-    set(key,value){
-        let index = this.hash(key)
-        if(!this.table[index]){
-            this.table[index] = []
-        }
-        for(let pair of this.table[index]){
-            if(pair[0]==key){
-                pair[1]=value
-                return
-            }
-        }
-        this.table[index].push([key,value])
-    }
-
-    get(key){
-        let index = this.hash(key)
-        let bucket = this.table[index]
-        for(let pair of bucket){
-            if(pair[0]==key){
-                return pair[1]
-            }
-        }
-    }
-
-
-
-    print() {
-        this.table.forEach((bucket, index) => {
-            if (bucket) {
-                console.log(`Index ${index}:`, bucket);
-            }
-        });
+    constructor(size=10) {
+        
     }
 }
-const hashTable = new HashTable()
-hashTable.set("rammm","kunna")
-hashTable.set("rammm","1")
-console.log("andi ondo kunna",hashTable.get("rammm"));
-hashTable.print()
-console.log(hashTable);
