1a2e4a71a78494448fe97b1471c7c075539655dc
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-12-15 21:16:40 +0530

    chnges

--- Stack/prac.js
+++ Stack/prac.js
@@ -16,11 +16,33 @@ class Stack {
         return this.items.pop()
     }
 
-    
     sizes(){
         return this.items.length
     }
 
 }
+
+function reverseString(str){
+    const stack = new Stack()
+
+    for(let char of str){
+        stack.push(char)
+    }
+    stack.print()
+
+    let largest = 0
+
+    for(i=0;i<str.length;i++){
+        if(stack.items[i]>largest){
+            largest = stack.items[i]
+        }
+    }
+    console.log(largest);
+    
+}
+
+let str = [1,2,3,4,9,5,6,7]
+
+reverseString(str)
 
 
