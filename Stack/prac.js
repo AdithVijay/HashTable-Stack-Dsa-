ff90c0167fbaa4d3cfb2fa26d7950d9e895954cb
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-12-15 18:17:50 +0530

    chnges

--- Stack/prac.js
+++ Stack/prac.js
@@ -1,57 +1,7 @@
-function merge(left,right,arr){
-    let leftLength = left.length;
-    let rightLength = right.length;
-
-    let l = 0;
-    let r =0;
-    let k=0;
-
-    while(l<leftLength && r<rightLength){
-        if(left[l]<right[r]){
-            arr[k]= left[l];
-            l++;
-        }else{
-            arr[k] = right[r];
-            r++;
-        }
-        k++;
+class Stack {
+    constructor(size = 10) {
+        this.items = []
+        this.size = 0
     }
-    while(l<leftLength){
-        arr[k] = left[l];
-        k++;
-        l++;
-    }
-    while(r<rightLength){
-        arr[k] = right[r];
-        k++;
-        r++;
-    }
-}
-
-
-function mergeSort(arr){
-    if(arr.length<2){
-        return arr;
-    }
-    let mid  = Math.floor(arr.length/2);
-   let  left = [];
-   let right = [];
-    let k=0;
-
     
-
-    for(let i=0;i<mid;i++){
-        left[i] = arr[i];
-    }
-    for(let j=mid;j<arr.length;j++){
-        right[k] = arr[j];
-        k++;
-    }
-
-    mergeSort(left);
-    mergeSort(right);
-    merge(left,right,arr)
-    return arr;
 }
-
-console.log(mergeSort(arr))
