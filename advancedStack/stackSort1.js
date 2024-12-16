b7c98ab7a1ad44ad6b2e7f91fe36f270c85f7afb
AdithVijay <144526699+AdithVijay@users.noreply.github.com>
2024-12-16 11:50:38 +0530

    chng

---
+++ advancedStack/stackSort1.js
@@ -0,0 +1,45 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    push(data) {
+        this.items.push(data);
+    }
+
+    pop() {
+        return this.items.pop();
+    }
+
+    peek() {
+        return this.items[this.items.length - 1];
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+
+    print() {
+        console.log(this.items.join(" => "));
+    }
+}
+
+function sortStackWithAuxiliary(originalStack) {
+    const auxilastack = new Stack()
+    
+}
+
+// Test
+const stack = new Stack();
+stack.push(3);
+stack.push(1);
+stack.push(4);
+stack.push(2);
+
+console.log("Original Stack:");
+stack.print();
+
+sortStackWithAuxiliary(stack);
+
+console.log("Sorted Stack:");
+stack.print();
